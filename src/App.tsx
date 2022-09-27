216d13b7c3e6a8be0c78af53f4f2a7576c62b669
Andry R Santos <andryrsantos@MacBook-Air-de-Andry.local>
2022-09-27 19:45:02 -0300

    feat: arquive app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import {Routes} from '@routes';
+
+import {Providers} from '@/provider';
+
+function App() {
+  return (
+    <Providers>
+      <Routes />
+    </Providers>
+  );
+}
+
+export default App;
