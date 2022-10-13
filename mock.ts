7989353ff7b8f33038702216958764661c1e61be
Andry R Santos <anrodriguessantos0@gmail.com>
2022-10-12 22:39:49 -0300

    feat:test

---
+++ mock.ts
@@ -0,0 +1,379 @@
+import uuid from 'react-native-uuid';
+
+export const MockTickets = [
+  {
+    category: 'Humor',
+    data: [
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '15 Set',
+        priceUnit: '69,00',
+        title: 'Stand Up Raiz',
+        shortAddress: 'Teatro Satander, Av Paulista - SP',
+        banner: 'https://i.ibb.co/cygPXt3/banner-danilo.jpg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '26 Dez',
+        priceUnit: '39,00',
+        title: 'Benignismo',
+        shortAddress: 'Teatro Ralino Zamboto, Itatibaia - SP',
+        banner: 'https://i.ibb.co/3WS36Lp/62dae94e52f51-xs.png',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '11 Out',
+        priceUnit: '29,00',
+        title: 'COMEDIA NOVA STAND UP COMEDY',
+        shortAddress: 'O Boteco do Xexeu, Sao Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcuXiWWRL3fKGOmHkne-ypkkcjVk-nDWB3nH1OVZLPOA&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '8 Out',
+        priceUnit: '59,00',
+        title: 'PERUCAS COMEDY',
+        shortAddress: 'Hamsa Comedy, Parque Paineiras - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwo-2cI55bLhP0GIqEijne_tAFk1pVgtt4T794Oa8VoLxFpzhGreB7zdw1KqOV&s',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '15 Set',
+        priceUnit: '69,00',
+        title: 'Stand Up Raiz',
+        shortAddress: 'Teatro Satander, Av Paulista - SP',
+        banner: 'https://i.ibb.co/cygPXt3/banner-danilo.jpg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '26 Dez',
+        priceUnit: '39,00',
+        title: 'Benignismo',
+        shortAddress: 'Teatro Ralino Zamboto, Itatibaia - SP',
+        banner: 'https://i.ibb.co/3WS36Lp/62dae94e52f51-xs.png',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '11 Out',
+        priceUnit: '29,00',
+        title: 'COMEDIA NOVA STAND UP COMEDY',
+        shortAddress: 'O Boteco do Xexeu, Sao Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcuXiWWRL3fKGOmHkne-ypkkcjVk-nDWB3nH1OVZLPOA&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Stand Up',
+        date: '8 Out',
+        priceUnit: '59,00',
+        title: 'PERUCAS COMEDY',
+        shortAddress: 'Hamsa Comedy, Parque Paineiras - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwo-2cI55bLhP0GIqEijne_tAFk1pVgtt4T794Oa8VoLxFpzhGreB7zdw1KqOV&s',
+      },
+    ],
+  },
+  {
+    category: 'Shows',
+    data: [
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '79,00',
+        title: 'David Garret',
+        shortAddress: 'Espaço Unimed, Barra Funda - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzivnJ8HuwL-IThu2wHw-jdiW-nxQs9UCFc1XJ6B6SlTXAXshgkVMh3p6VsQ&s',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '31 Dez',
+        priceUnit: '99,00',
+        title: 'HARRY STYLES - LOVE ON TOUR',
+        shortAddress: 'Allianz Parque, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa6l3TaFhbChk8F9-pywhISzPrd4z4aSwtABDH60dc94zDDJAk_V_bERkVYw&s=10',
+      },
+
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '89,00',
+        title: 'ROBERTO CARLOS',
+        shortAddress: 'Allianz Parque, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx-1Ombv0qUDtCZxK7eID2KmUtpaLNGz5YMuu-GZbZG6Izrd3MGm37st0Auw&s=10',
+      },
+
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '89,00',
+        title: 'Black Label Society',
+        shortAddress: 'Audio, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiST6pVKsm-041QTvzXBgAPdR-yku-O9kGazQCL1y6P4Ih&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '79,00',
+        title: 'David Garret',
+        shortAddress: 'Espaço Unimed, Barra Funda - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzivnJ8HuwL-IThu2wHw-jdiW-nxQs9UCFc1XJ6B6SlTXAXshgkVMh3p6VsQ&s',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '31 Dez',
+        priceUnit: '99,00',
+        title: 'HARRY STYLES - LOVE ON TOUR',
+        shortAddress: 'Allianz Parque, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTa6l3TaFhbChk8F9-pywhISzPrd4z4aSwtABDH60dc94zDDJAk_V_bERkVYw&s=10',
+      },
+
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '89,00',
+        title: 'ROBERTO CARLOS',
+        shortAddress: 'Allianz Parque, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSx-1Ombv0qUDtCZxK7eID2KmUtpaLNGz5YMuu-GZbZG6Izrd3MGm37st0Auw&s=10',
+      },
+
+      {
+        id: uuid.v4().toString(),
+        category: 'Musicais',
+        date: '8 Out',
+        priceUnit: '89,00',
+        title: 'Black Label Society',
+        shortAddress: 'Audio, Água Branca - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiST6pVKsm-041QTvzXBgAPdR-yku-O9kGazQCL1y6P4Ih&s=10',
+      },
+    ],
+  },
+  {
+    category: 'Tecnologia e Inovaçoes',
+    data: [
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '15 Out',
+        priceUnit: '29,00',
+        title: 'Futurecom',
+        shortAddress: 'São Paulo Expo, Jabaquara - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhzYQUZQeGSCoB-SecEwcX1D-SKWe8-vlo0OoBuiRuNFg&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '08 Out',
+        priceUnit: '19,00',
+        title: 'Campus Party Brasil',
+        shortAddress: 'Grand Plaza Shopping, Santo André - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfL2A1ZZxr_iFt-3ypxc1irsuzRVZsr90ozfyV7UuiPscB4o-3f2y57jtAEg&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '18 Out',
+        priceUnit: '49,00',
+        title: 'DNGX Conference 2022',
+        shortAddress: 'Grand Plaza Shopping, Santo André - SP',
+        banner:
+          'https://media-exp1.licdn.com/dms/image/C4D22AQFoo4B6-ARYag/feedshare-shrink_2048_1536/0/1665015468479?e=2147483647&v=beta&t=DjBGAI-bvZyuhdPWvBNVXUk-B5j-glnFfbyh1OcIIQo',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '18 Out',
+        priceUnit: '00,00',
+        title: 'Insurtech Brasil 2023',
+        shortAddress: 'Itaú Cultura, São Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfdf-Ss1ylgQWjIAo4Ss9KgM14jItg05TVMBsMPY8Szg&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '15 Out',
+        priceUnit: '29,00',
+        title: 'Futurecom',
+        shortAddress: 'São Paulo Expo, Jabaquara - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRQhzYQUZQeGSCoB-SecEwcX1D-SKWe8-vlo0OoBuiRuNFg&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '08 Out',
+        priceUnit: '19,00',
+        title: 'Campus Party Brasil',
+        shortAddress: 'Grand Plaza Shopping, Santo André - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfL2A1ZZxr_iFt-3ypxc1irsuzRVZsr90ozfyV7UuiPscB4o-3f2y57jtAEg&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '18 Out',
+        priceUnit: '49,00',
+        title: 'DNGX Conference 2022',
+        shortAddress: 'Grand Plaza Shopping, Santo André - SP',
+        banner:
+          'https://media-exp1.licdn.com/dms/image/C4D22AQFoo4B6-ARYag/feedshare-shrink_2048_1536/0/1665015468479?e=2147483647&v=beta&t=DjBGAI-bvZyuhdPWvBNVXUk-B5j-glnFfbyh1OcIIQo',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tecnologia',
+        date: '18 Out',
+        priceUnit: '00,00',
+        title: 'Insurtech Brasil 2023',
+        shortAddress: 'Itaú Cultura, São Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfdf-Ss1ylgQWjIAo4Ss9KgM14jItg05TVMBsMPY8Szg&s=10',
+      },
+    ],
+  },
+  {
+    category: 'Negócios',
+    data: [
+      {
+        id: uuid.v4().toString(),
+        category: 'Empresarial',
+        date: '15 Nov',
+        priceUnit: '00,00',
+        title: 'Design Thinking para tudo',
+        shortAddress: 'ESPM, São Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgBFNKAkCfDmQP41xCJLNuWsijWZzhwsvogXVqRdS6HtH90W9RKkR_zKN9zljv&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Empresarial',
+        date: '08 Dez',
+        priceUnit: '19,00',
+        title: 'ExpoISP Brasil (Expo ISP Brasil)',
+        shortAddress: 'PRO MAGNO Centro de Eventos, São Paulo - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsvg3T9BwDv-xY2HxuWtYKvF4OiLk5fv_ezf4brqcu2hh9aLF5eguwGSWJgVZzoQ&s=10',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Empresarial',
+        date: '10 Out',
+        priceUnit: '00,00',
+        title: 'II Congresso CIEES',
+        shortAddress: 'Grand Plaza Shopping, Santo André - SP',
+        banner:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQgqbvFDL2jjR7eQ52gUbnlNrg4ZUNWa0Xwt0QOvKWewOnb&s',
+      },
+    ],
+  },
+  {
+    category: 'Passeios e tours',
+    data: [
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '20 Set',
+        priceUnit: '190,00',
+        title: 'Sampa Sky',
+        shortAddress: 'Sampa Sky, São Paulo - SP',
+        banner:
+          'https://assets.bileto.sympla.com.br/eventmanager/production/nfke9gufrmnbmc9ep0o0s3ccfa2cm4deb6utinfs7ps7kh7scseh5ng07lrop2947ok2815s105a692jnktcsiog83h61u39712vq1.jpeg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '20 Set',
+        priceUnit: '19,00',
+        title: 'Museu Lingua Portuguesa',
+        shortAddress: 'Museu da Lingua Portuguesa, São Paulo - SP',
+        banner:
+          'https://assets.bileto.sympla.com.br/eventmanager/production/29c78jcoa89qhfmtgq8rkaaeve42pmgkmjkjtkv8mo5h7jlg3fq3dq4hbpel7b0sr4u5ve4ke1tuhvg832uk8th9sjj3pvj8qbjqf6l.jpeg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '12 Out',
+        priceUnit: '99,00',
+        title: 'Parque Maeda',
+        shortAddress: 'Cote tour, Santos - SP',
+        banner: 'https://images.sympla.com.br/62b1e6740bc0c-xs.jpg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '24 SET',
+        priceUnit: '50,00',
+        title: 'River Acqua Park',
+        shortAddress: 'River Park, Santa Bárbara - MG',
+        banner:
+          'https://bhaz.com.br/wp-content/uploads/2017/08/tobo%C3%A1gua-1024x555.jpg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '13 Nov',
+        priceUnit: '50,00',
+        title: 'Escuna Ilha Grande',
+        shortAddress: 'Ilha Grande, Angra dos Reis - Rj',
+        banner: 'https://images.sympla.com.br/631f9b7edb5d3-xs.jpg',
+      },
+      {
+        id: uuid.v4().toString(),
+        category: 'Tour',
+        date: '13 Nov',
+        priceUnit: '50,00',
+        title: 'Ilha da Gigoia',
+        shortAddress: 'Ilha da Gigoia, Rio de Janeiro - Rj',
+        banner: 'https://images.sympla.com.br/6326121e75ba4-xs.jpg',
+      },
+    ],
+  },
+];
+
+export const MockCategories = [
+  {
+    name: 'Musicais',
+    img: 'https://i.ibb.co/pLPtWfG/4-A2-B7-B-4.png',
+  },
+  {
+    name: 'Stand Up',
+    img: 'https://i.ibb.co/wwCVz1V/4-A2-B7-B-3.png',
+  },
+  {
+    name: 'Tecnologia',
+    img: 'https://i.ibb.co/hfnGBzd/4-A2-B7-B-2.png',
+  },
+  {
+    name: 'Negocios',
+    img: 'https://i.ibb.co/BPrfGJy/4-A2-B7-B-1.png',
+  },
+  {
+    name: 'Tour e passeios',
+    img: 'https://i.ibb.co/HrwGhcw/4A2B7B.png',
+  },
+];
