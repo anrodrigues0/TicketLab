ded05a0b47d7cfcbe4005cff27611cfe078cbc5b
Andry R Santos <anrodriguessantos0@gmail.com>
2022-11-03 23:00:55 -0300

    test: logo component

--- src/components/Logo/Logo.test.tsx
+++ src/components/Logo/Logo.test.tsx
@@ -1,9 +1,14 @@
+import {describe, test} from '@jest/globals';
 import {render} from '@testing-library/react-native';
+
 import {Logo} from './Logo';
+import {CommonsStrings} from '../../common/strings';
 
 describe('Component Logo', () => {
-  test('Test', () => {
-    const {debug} = render(<Logo />);
-    debug();
+  test('Should show Logo on screen', () => {
+    const {getByText} = render(<Logo />);
+
+    expect(getByText(CommonsStrings.components.Logo.ticket)).toBeTruthy();
+    expect(getByText(CommonsStrings.components.Logo.lab)).toBeTruthy();
   });
 });
--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,11 @@
 import {Container, Text} from './Logo.style';
+import {CommonsStrings} from '../../common/strings';
 
 export function Logo() {
   return (
     <Container>
-      <Text>Ticket</Text>
-      <Text yellow>Lab</Text>
+      <Text>{CommonsStrings.components.Logo.ticket}</Text>
+      <Text yellow>{CommonsStrings.components.Logo.lab}</Text>
     </Container>
   );
 }
